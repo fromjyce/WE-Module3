d2243490e0300d6cbc1cb75297a957e8b1046b9a
jaya2 <jaya2004kra@gmail.com>
2024-04-01 20:22:42 +0530

    added splash screen

--- GenAI-Assignment-6/DiamondBidding.py
+++ GenAI-Assignment-6/DiamondBidding.py
@@ -1,15 +1,53 @@
 import pygame
 import sys
+import time
 
 pygame.init()
 
-width = 950
-height = 750
+#screen width
+width = 800
+height = 600
 
-background_color = (13, 116, 47)  # usual green colour
+background_color = (13, 116, 47)  # green
 
+#screen
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Diamond Bidding Game")
+pygame.display.set_caption("My Pygame Window")
+
+#images for splash screen
+image1 = pygame.image.load(r'C:\Users\jaya2\Visual Code\Module3\GenAI-Assignment-6\Images\Name.png')
+image2 = pygame.image.load(r'C:\Users\jaya2\Visual Code\Module3\GenAI-Assignment-6\Images\PyGame.png')
+
+image1_width = 250
+image1_height = 250
+image2_width = 315
+image2_height = 175
+
+image1 = pygame.transform.scale(image1, (image1_width, image1_height))
+image2 = pygame.transform.scale(image2, (image2_width, image2_height))
+
+
+def splash_screen():
+    screen.fill(background_color)
+
+    screen.blit(image1, ((width - image1_width) // 2, (height - image1_height) // 4))
+    screen.blit(image2, ((width - image2_width) // 2, (height - image2_height) // 4 * 3))
+
+    pygame.display.flip()
+    time.sleep(10)
+
+def main_screen():
+    screen.fill(background_color)
+    
+    font = pygame.font.SysFont(None, 48)
+    text = font.render("Hello World", True, (255, 255, 255))  # White text
+    text_rect = text.get_rect(center=(width // 2, height // 2))
+    screen.blit(text, text_rect)
+
+    pygame.display.flip()
+
+
+splash_screen()
 
 running = True
 while running:
@@ -17,9 +55,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill(background_color)
+    main_screen()
 
-    pygame.display.flip()
 
 pygame.quit()
 sys.exit()
