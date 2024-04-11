68b622d90a72b415d57381856426d9bed7365cde
jaya2 <jaya2004kra@gmail.com>
2024-04-11 22:00:19 +0530

    added hearts code

--- GenAI-Assignment-6/DiamondBidding.py
+++ GenAI-Assignment-6/DiamondBidding.py
@@ -204,13 +204,111 @@ def spades_page():
 
 
 def hearts_page():
+    current_card_index = 0
     screen.fill(background_color)
     font = pygame.font.SysFont(None, 42)
     text = font.render("Choose a Card", True, (0, 0, 0))
-    text_rect = text.get_rect(center=(width // 2, height // 2 - 250))
+    text_rect = text.get_rect(center=(width // 2, height // 2 - 240))
     screen.blit(text, text_rect)
 
+    cards = diamond_cards_shuffle()
+
+    def display_current_card():
+        revealed_card_width = 125
+        revealed_card_height = 175
+        if current_card_index < len(cards):
+            revealed_card = cards[current_card_index]
+            diamond_card_image = pygame.image.load(r"C:\Users\jaya2\Visual Code\Module3\GenAI-Assignment-6\Cards\Diamonds\{}.png".format(revealed_card))
+            diamond_card_image = pygame.transform.scale(diamond_card_image, (revealed_card_width, revealed_card_height))
+            diamond_card_button_rect = diamond_card_image.get_rect(center=(width // 2 - 260, height // 2 - 100))
+            screen.blit(diamond_card_image, diamond_card_button_rect)
+            pygame.display.flip()
+        else:
+            pass
+
+    display_current_card()
+
+    # Display next button
+    home_button_image = pygame.image.load(r"C:\Users\jaya2\Visual Code\Module3\GenAI-Assignment-6\Images\HomeButton.png")
+    home_button_image = pygame.transform.scale(home_button_image, (200, 200))
+    home_button_image_rect = home_button_image.get_rect(center=(width // 2 - 260, height // 2 + 200))
+    screen.blit(home_button_image, home_button_image_rect)
+
+    display_card_width = 90
+    display_card_height = 140
+
+    selected_card_image = pygame.image.load(r"C:\Users\jaya2\Visual Code\Module3\GenAI-Assignment-6\Images\Joker.png")
+    selected_card_image = pygame.transform.scale(selected_card_image, (display_card_width,display_card_height))
+
+    card_rects = {}
+
+    card_positions = [((width // 2 - 90, height // 2 - 115), "A"), ((width // 2 + 15, height // 2 - 115), "2"), ((width // 2 + 120, height // 2 - 115), "3"), ((width // 2 + 225, height // 2 - 115), "4"), ((width // 2 + 330, height // 2 - 115), "5"),
+                      ((width // 2 - 90, height // 2 + 40), "6"), ((width // 2 + 15, height // 2 + 40), "7"), ((width // 2 + 120, height // 2 + 40), "8"), ((width // 2 + 225, height // 2 + 40), "9"), ((width // 2 + 330, height // 2 + 40), "10"), 
+                      ((width // 2 - 90, height // 2 + 195), "J"), ((width // 2 + 15, height // 2 + 195), "Q"), ((width // 2 + 120, height // 2 + 195), "K")]
+    
+    for position, card_value in card_positions:
+        card_image = pygame.image.load(r"C:\Users\jaya2\Visual Code\Module3\GenAI-Assignment-6\Cards\Spades\{}.png".format(card_value))
+        card_image = pygame.transform.scale(card_image, (display_card_width, display_card_height))
+        card_rect = card_image.get_rect(center=(position))
+        card_rects[card_value] = card_rect
+        screen.blit(card_image, card_rect)
+
     pygame.display.flip()
+
+    button_ranges = {
+        (range(266, 350), range(118, 244)) : "A",
+        (range(378, 450), range(119, 247)) : 2, 
+        (range(478, 558), range(123, 247)) : 3,
+        (range(582, 666), range(117, 251)) : 4,
+        (range(688, 772), range(119, 246)) : 5,
+        (range(267, 354), range(276, 404)) : 6,
+        (range(374, 457), range(277, 402)) : 7,
+        (range(481, 560), range(278, 404)) : 8,
+        (range(587, 665), range(275, 404)) : 9,
+        (range(690, 771), range(275, 402)) : 10,
+        (range(270, 352), range(431, 560)) : "J",
+        (range(375, 458), range(429, 560)) : "Q",
+        (range(482, 560), range(431, 564)) : "K"
+    }
+
+    home_button_range_x = range(81, 199)
+    home_button_range_y = range(477, 520)
+
+    human_card = 0
+    revealed_score_card = 0
+    computer_card = 0
+
+    click_count = 0
+
+    human_score = 0
+    computer_score = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                #print("Mouse Position:", mouse_x, mouse_y)
+                for button_range, card_value in button_ranges.items():
+                    if mouse_x in button_range[0] and mouse_y in button_range[1]:
+                        human_card = card_value
+                        if current_card_index < len(cards):
+                            current_card_index += 1
+                            computer_card = computer_card_choice(cards[current_card_index - 1])
+                            revealed_score_card = cards[current_card_index - 1]
+                            display_current_card()
+                            screen.blit(selected_card_image, card_rects[str(human_card)])
+                            pygame.display.flip()
+                            click_count += 1
+                            human_score, computer_score = add_score(human_score, computer_score, human_card, computer_card, revealed_score_card)
+                            if click_count == 13:
+                                display_winners(human_score, computer_score)
+                        #print("HUMAN: {h}; COMPUTER: {c}; Revealed: {r}".format(h=human_card, c=computer_card, r=revealed_score_card))
+                        break
+                if mouse_x in home_button_range_x and mouse_y in home_button_range_y:
+                    main_screen()
 
 def clubs_page():
     screen.fill(background_color)
